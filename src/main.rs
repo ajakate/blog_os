d478a9f22d7006e3206c51d01a77fa19efcca9b1
Ajay Jakate <ajay.jakate@gmail.com>
2021-02-13 16:15:30 -0600

    safer vga io; println macro

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,14 @@ mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello Ajay!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
+    println!("Hello World{} {}", "!");
+    panic!("Some panic message");
     loop {}
 }
